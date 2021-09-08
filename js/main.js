3b7ddaadc38f26f20a35da23fc17efaffeb358c7
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-08 15:49:55 -0500

    Se hace una prueba para la creacion de elementos de una tabla automaticamente

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 document.getElementById("petsave-button").onclick = function () {
 	
 	let pet ;
+
+// se crea el metodo que filtra las fablas
+
+function insertPetTable(){
+	var Tbl = document.getElementById("table");
+	var tblBody = Tbl.getElementsByTagName("tbody");
+	var textoCelda = document.createTextNode("celda");
+
+	celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    tblBody.appendChild(hilera);
+    Tbl.appendChild(tblBody);
+
+
+}
