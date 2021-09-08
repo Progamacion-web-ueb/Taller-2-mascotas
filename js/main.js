23b69e69465cfa4f63e4ef138b4ac588ca483fc8
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-08 17:27:01 -0500

    Se crear un script que agrega una nueva fila

--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
-document.getElementById("petsave-button").onclick = function () {
-	
-	let pet ;
+
 
 // se crea el metodo que filtra las fablas
+console.log("js on")
+function genera_tabla() {
+	 var body = document.getElementsByTagName("body")[0];
 
-function insertPetTable(){
-	var Tbl = document.getElementById("table");
-	var tblBody = Tbl.getElementsByTagName("tbody");
-	var textoCelda = document.createTextNode("celda");
+  // Crea un elemento <table> y un elemento <tbody>
+  var tabla   = document.createElement("table");
+  var tblBody = document.createElement("tbody");
 
-	celda.appendChild(textoCelda);
-    hilera.appendChild(celda);
+  // Crea las celdas
+  for (var i = 0; i < 2; i++) {
+    // Crea las hileras de la tabla
+    var hilera = document.createElement("tr");
+
+    for (var j = 0; j < 2; j++) {
+      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+      // texto sea el contenido de <td>, ubica el elemento <td> al final
+      // de la hilera de la tabla
+      var celda = document.createElement("td");
+      var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+      celda.appendChild(textoCelda);
+      hilera.appendChild(celda);
+    }
+
+    // agrega la hilera al final de la tabla (al final del elemento tblbody)
     tblBody.appendChild(hilera);
-    Tbl.appendChild(tblBody);
+    }
 
+  // posiciona el <tbody> debajo del elemento <table>
+  tabla.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tabla);
+  // modifica el atributo "border" de la tabla y lo fija a "2";
+  tabla.setAttribute("border", "2");
+}
 
+function insertRow() {
+	console.log("inset row up");
+	var table = document.getElementById('table');
+	var tbody = document.getElementById('informationTable');
+	var rowPet = document.createElement("tr");
+	var celPet = document.createElement("td");
+	var informationPet = document.createTextNode("celda en la hilera "+i+", columna ");
+	for (var i = 0;i<8 ;i++) {
+		celPet.appendChild(informationPet);
+		console.log("for"+i);
+	}
+	rowPet.appendChild(celPet);
+	tbody.appendChild(rowPet);
+	table.appendChild(tbody);
+
+}
+function addrow(){
+ var table = document.getElementById("table");
+ var row = table.insertRow(2);
+  //this adds row in 0 index i.e. first place
+ row.innerHTML = "<td>row0 column1</td><td>row0 column2</td>";
 }
