fa842730a6005161c60c7ff5b51129105aa3de05
TenshiGC <tauroangel.ruiz@gmail.com>
2021-09-09 01:25:14 -0500

    Se pueden añadir elementos a la tabla, cambio en la estructura de la tabla e inicio modificacion JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
 document.getElementById("petsave-button").onclick = function () {
-	
-	let pet ;
+	let pet = {
+		ownerInput: document.getElementById("ownerName-input").value,
+		petNameInput: document.getElementById("petName-input").value,
+		microchipInput: document.getElementById("microchip-input").value,
+		speciesInput: document.getElementById("species-input").value,
+		sexInput: document.getElementById("sex-input").value,
+		sizeInput: document.getElementById("size-input").value,
+		sterilizationInput: document.getElementById("sterilization-input").value,
+		dangerousInput: document.getElementById("dangerous-input").value,
+		neighborhoodInput: document.getElementById("neighborhood-input").value,
+	}
+	let tr = document.createElement("tr");
+
+	Object.keys(pet).forEach((key) => {
+		console.log(key);
+
+		let td = document.createElement("td");
+		td.innerHTML = pet[key];
+
+		tr.appendChild(td);
+	});
+
+	document.getElementById("body-table").appendChild(tr);
+};
