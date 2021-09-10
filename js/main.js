9f57b227479f79ad2530d8792e44f8c6e69ff739
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-10 17:19:24 -0500

    bse de datoss
    Se agrega la funcionalidad de guardar datos en el navegador

--- js/main.js
+++ js/main.js
@@ -1,60 +1,61 @@
 
 
-// se crea el metodo que filtra las fablas
-console.log("js on")
-function genera_tabla() {
-	 var body = document.getElementsByTagName("body")[0];
+var rowId = 0;
 
-  // Crea un elemento <table> y un elemento <tbody>
-  var tabla   = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+const dbName = "petDB";
 
-  // Crea las celdas
-  for (var i = 0; i < 2; i++) {
-    // Crea las hileras de la tabla
-    var hilera = document.createElement("tr");
+var request = indexedDB.open(dbName, 2);
 
-    for (var j = 0; j < 2; j++) {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
-      var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
-    }
-
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
-    tblBody.appendChild(hilera);
-    }
-
-  // posiciona el <tbody> debajo del elemento <table>
-  tabla.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tabla);
-  // modifica el atributo "border" de la tabla y lo fija a "2";
-  tabla.setAttribute("border", "2");
-}
-
-function insertRow() {
-	console.log("inset row up");
-	var table = document.getElementById('table');
-	var tbody = document.getElementById('informationTable');
-	var rowPet = document.createElement("tr");
-	var celPet = document.createElement("td");
-	var informationPet = document.createTextNode("celda en la hilera "+i+", columna ");
-	for (var i = 0;i<8 ;i++) {
-		celPet.appendChild(informationPet);
-		console.log("for"+i);
+request.onerror = function (event) {
+	console.log("Database error");
+};
+request.onupgradeneeded = function (event) {
+	var db = event.target.result;
+	var objectStore = db.createObjectStore("pets", { keyPath: "ownername" });
+	objectStore.createIndex("petNameInput", "petNameInput", { unique: false });
+};
+var request = indexedDB.open(dbName, 2);
+request.onsuccess = function (event) {
+	var db = event.target.result;
+	var tx = db.transaction("pets");
+	var objectStore = tx.objectStore("pets");
+	objectStore.getAll().onsuccess = function (event) {
+		console.log(event.target.result);
+		rowId = event.target.result.length;
+	};
+};
+document.getElementById("petsave-button").onclick = function () {
+	rowId += 1;
+	let pet = {
+		ownerInput: document.getElementById("ownerName-input").value,
+		petNameInput: document.getElementById("petName-input").value,
+		microchipInput: document.getElementById("microchip-input").value,
+		speciesInput: document.getElementById("species-input").value,
+		sexInput: document.getElementById("sex-input").value,
+		sizeInput: document.getElementById("size-input").value,
+		sterilizationInput: document.getElementById("sterilization-input").value,
+		dangerousInput: document.getElementById("dangerous-input").value,
+		neighborhoodInput: document.getElementById("neighborhood-input").value,
 	}
-	rowPet.appendChild(celPet);
-	tbody.appendChild(rowPet);
-	table.appendChild(tbody);
 
-}
-function addrow(){
- var table = document.getElementById("table");
- var row = table.insertRow(2);
-  //this adds row in 0 index i.e. first place
- row.innerHTML = "<td>row0 column1</td><td>row0 column2</td>";
-}
+	var request = indexedDB.open(dbName, 2);
+	request.onsuccess = function (event) {
+		var db = event.target.result;
+		var customerObjectStore = db.transaction("pets", "readwrite").objectStore("pets");
+		pet["ownername"] = rowId;
+		customerObjectStore.add(pet);
+	};
+
+	let tr = document.createElement("tr");
+
+	Object.keys(pet).forEach((key) => {
+		console.log(key);
+
+		let td = document.createElement("td");
+		td.innerHTML = pet[key];
+
+		tr.appendChild(td);
+	});
+	document.getElementById("body-table").appendChild(tr);
+};
