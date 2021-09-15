5939b719ec124b4e83f1c4b19414e7a063b152e5
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-15 14:39:48 -0500

    Se agrego parte de la funcionalidad del filtrado

--- js/createDb.js
+++ js/createDb.js
@@ -1,5 +1,9 @@
 
 	var key;
+
+	document.querySelector("#refresh").addEventListener("click", reload);
+	document.querySelector("#butonFilter").addEventListener("click", filter);
+
     console.log("create data base");
 	var openRequest = indexedDB.open("pets",1);
 	openRequest.onupgradeneeded = function(e) {
@@ -13,6 +17,9 @@
 		db = e.target.result;
 		//Listen for add clicks
 		document.querySelector("#addDataTable").addEventListener("click", addTable);
+		//Listen for add clicks
+		
+		
 		
 	}	
 	openRequest.onerror = function(e) {
@@ -21,16 +28,7 @@
 	}
 
 	function addTable(){
-		
-	
-		//codigo util para lee un solo dato
-		var transaction = db.transaction(["people"],"readonly");
-		var store = transaction.objectStore("people");
-		store.get(6).onsuccess = function(event) {
-			var nombre = event.target.result;
-			//console.log(nombre.name);
-		}
-	
+
 		var tx = db.transaction(["people"],"readonly");
 		var store = tx.objectStore('people');
 		store.openCursor().onsuccess = function(event) {
@@ -56,7 +54,6 @@
 				document.getElementById("body-table").appendChild(tr);
 				key=cursor.key;
 
-				
 			
 				tr.appendChild(td1);
 				tr.appendChild(td2);
@@ -69,8 +66,7 @@
 				tr.appendChild(td9);
 				tr.appendChild(td10);
 				tr.appendChild(td11);
-						
-				console.log(current.name);
+
 				td1.innerHTML= current.ownerName;
 				td2.innerHTML= current.petName;
 				td3.innerHTML= current.microchip;
@@ -83,16 +79,88 @@
 				td10.innerHTML= current.creationDate;
 				td11.innerHTML= key;
 
-				
 				// Pasamos a procesar el siguiente resultado
 				cursor.continue();
 			
 			}
 		} 
-		
+	}
+
+	function reload(){
+		location.reload();
+	}
+
+	function filter(){
+		var tx = db.transaction(["people"],"readonly");
+		var store = tx.objectStore('people');
+		store.openCursor().onsuccess = function(event) {
+			var cursor = event.target.result;
+			// cursor será truthy mientras haya elementos que procesar
+			if (cursor) {
+				// En cursor.value tenemos el elemento actual
+				var current = cursor.value;
+
+				let td1 = document.createElement("td");
+				let td2 = document.createElement("td");
+				let td3 = document.createElement("td");
+				let td4 = document.createElement("td");
+				let td5 = document.createElement("td");
+				let td6 = document.createElement("td");
+				let td7 = document.createElement("td");
+				let td8 = document.createElement("td");
+				let td9 = document.createElement("td");
+				let td10 = document.createElement("td");
+				let td11 = document.createElement("td");
+				let tr = document.createElement("tr");
+				document.getElementById("body-table").appendChild(tr);
+				key=cursor.key;
+			
+				tr.appendChild(td1);
+				tr.appendChild(td2);
+				tr.appendChild(td3);
+				tr.appendChild(td4);
+				tr.appendChild(td5);
+				tr.appendChild(td6);
+				tr.appendChild(td7);
+				tr.appendChild(td8);
+				tr.appendChild(td9);
+				tr.appendChild(td10);
+				tr.appendChild(td11);
+
+				var filterneighborhood=document.querySelector("#neighborhoodOption").value;
+				var filterSize=document.querySelector("#sizeOption").value;
+				var filterDangerous=document.querySelector("#dangerousOption").value;
+				console.log(filterneighborhood);
+				console.log(filterSize);
+				console.log(filterDangerous);
+
+				if(filterneighborhood ==current.neighborhood){
+					console.log("filtro localidad on");
+					if(filterSize==current.size){
+						console.log("filtro size on");
+						if(filterDangerous==current.dangerous){
+							console.log("filtro size on");
+						}
+					}
+				}
+
+				td1.innerHTML= current.ownerName;
+				td2.innerHTML= current.petName;
+				td3.innerHTML= current.microchip;
+				td4.innerHTML= current.species;
+				td5.innerHTML= current.sex;
+				td6.innerHTML= current.size;
+				td7.innerHTML= current.sterile;
+				td8.innerHTML= current.dangerous;
+				td9.innerHTML= current.neighborhood;
+				td10.innerHTML= current.creationDate;
+				td11.innerHTML= key;
+
+				// Pasamos a procesar el siguiente resultado
+				cursor.continue();
+			
+			}
+		} 
 	}
 	
-	function alerta(){
-		alert("Submit button clicked!");
-	}
 
--- js/modifyDb.js
+++ js/modifyDb.js
@@ -1,24 +1,32 @@
-    console.log("create data base");
-    var openRequest = indexedDB.open("pets",1);
-    openRequest.onupgradeneeded = function(e) {
-        var thisDB = e.target.result;
-        if(!thisDB.objectStoreNames.contains("people")) {
-            thisDB.createObjectStore("people", {autoIncrement:true});
-        }
+var key;
+console.log("create data base");
+var openRequest = indexedDB.open("pets",1);
+openRequest.onupgradeneeded = function(e) {
+    var thisDB = e.target.result;
+    if(!thisDB.objectStoreNames.contains("people")) {
+        thisDB.createObjectStore("people", {autoIncrement:true});
     }
-    openRequest.onsuccess = function(e) {
-        alert("	openRequest.onsuccess");
-        db = e.target.result;
-        //Listen for add clicks
-        //document.querySelector("#addPet").addEventListener("click", addPerson);
-        //Listen for get clicks
-        //document.querySelector("#getButton").addEventListener("click", getPerson);
-        //Listen for get clicks
-        //document.querySelector("#getAllButton").addEventListener("click", getPeople);
-        //Listen for get clicks
-        //document.querySelector("#deleteButton").addEventListener("click", deletePerson);
-    }	
-    openRequest.onerror = function(e) {
-        alert("openRequest.onerror");
-        //Do something for the error
+}
+openRequest.onsuccess = function(e) {
+    db = e.target.result;
+    //Listen for add clicks
+    document.querySelector("#addKey").addEventListener("click", modifiPet);
+}    
+openRequest.onerror = function(e) {
+    alert("openRequest.onerror");
+    //Do something for the error
+}
+
+function modifiPet(){
+    key= document.getElementById('keyPet');
+    console.log(key);
+    var transaction = db.transaction(["people"],"readonly");
+    var store = transaction.objectStore("people");
+    store.get(key).onsuccess = function(event) {
+    var pet = event.target.result;
+    console.log(pet.ownerName);
+    //document.Formadd.ownerName.value= 30;
     }
+}
+
+
